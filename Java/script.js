eecb0a4c97ab4ab61a5638cf29d5fd58113a7303
Cinomme <derka161@gmail.com>
2021-10-20 03:18:07 -0300

    Correction to ECMAScript standards

--- Java/script.js
+++ Java/script.js
@@ -1,6 +1,6 @@
 $(() => {
 
-  $("#new").click(function () {
+  $('#new').click(function () {
 
     global1 = 0;
 
@@ -47,15 +47,15 @@ $(() => {
       $('#global1').text(global1)
 
       $('#round1').text(0)
-      round1 = 0
+      round1 = 0;
     } else {
       global2 += round2
       $('#global2').text(global2)
 
       $('#round2').text(0)
-      round2 = 0
+      round2 = 0;
     }
-    player1 = !player1
+    player1 = !player1;
 
     if (global1 >= 100) {
       alert(`VICTOIRE DU JOUEUR 2 avec ${global1}`)
@@ -80,13 +80,13 @@ $(() => {
       switch (roll) {
         case 1:
           $('#DD').html('<img src="../Images/dé n°1.png" alt="Dé">');
-          round1 = 0
-          player1 = !player1
+          round1 = 0;
+          player1 = !player1;
 
           if (player1) {
             $('#player1').css('background-color', 'red')
             $('#player2').css('background-color', 'rgba(255, 255, 255, 0)');
-      
+
           } else {
             $('#player2').css('background-color', 'red');
             $('#player1').css('background-color', 'rgba(255, 255, 255, 0)');
@@ -95,23 +95,23 @@ $(() => {
           break;
         case 2:
           $('#DD').html('<img src="../Images/dé n°2.png" alt="Dé">');
-          round1 += 2
+          round1 += 2;
           break;
         case 3:
           $('#DD').html('<img src="../Images/dé n°3.png" alt="Dé">');
-          round1 += 3
+          round1 += 3;
           break;
         case 4:
           $('#DD').html('<img src="../Images/dé n°4.png" alt="Dé">');
-          round1 += 4
+          round1 += 4;
           break;
         case 5:
           $('#DD').html('<img src="../Images/dé n°5.png" alt="Dé">');
-          round1 += 5
+          round1 += 5;
           break;
         case 6:
           $('#DD').html('<img src="../Images/dé n°6.png" alt="Dé">');
-          round1 += 6
+          round1 += 6;
           break;
         default:
           $('#DD').html('<img src="../Images/dé n°1.png" alt="Dé">');
@@ -122,13 +122,13 @@ $(() => {
       switch (roll) {
         case 1:
           $('#DD').html('<img src="../Images/dé n°1.png" alt="Dé">');
-          round2 = 0
-          player1 = !player1
+          round2 = 0;
+          player1 = !player1;
 
           if (player1) {
             $('#player1').css('background-color', 'red');
             $('#player2').css('background-color', 'rgba(255, 255, 255, 0)');
-      
+
           } else {
             $('#player2').css('background-color', 'red');
             $('#player1').css('background-color', 'rgba(255, 255, 255, 0)');
@@ -136,28 +136,28 @@ $(() => {
           break;
         case 2:
           $('#DD').html('<img src="../Images/dé n°2.png" alt="Dé">');
-          round2 += 2
+          round2 += 2;
           break;
         case 3:
           $('#DD').html('<img src="../Images/dé n°3.png" alt="Dé">');
-          round2 += 3
+          round2 += 3;
           break;
         case 4:
           $('#DD').html('<img src="../Images/dé n°4.png" alt="Dé">');
-          round2 += 4
+          round2 += 4;
           break;
         case 5:
           $('#DD').html('<img src="../Images/dé n°5.png" alt="Dé">');
-          round2 += 5
+          round2 += 5;
           break;
         case 6:
           $('#DD').html('<img src="../Images/dé n°6.png" alt="Dé">');
-          round2 += 6
+          round2 += 6;
           break;
         default:
           $('#DD').html('<img src="../Images/dé n°1.png" alt="Dé">');
       }
-      $('#round2').text(round2)
+      $('#round2').text(round2);
     }
 
   })
